52b7fb13da79af842b4e3a24100879774ac740ae
eraserx99 <eraserx99@gmail.com>
2012-05-24 00:08:58 -0400

    Initial Commit

---
+++ TWA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TWA
+//
+//  Created by CHIH-CHUN CHIEN on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TWA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TWA
+//
+//  Created by CHIH-CHUN CHIEN on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
